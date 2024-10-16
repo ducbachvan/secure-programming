132a8e3fee970187c3cfd9034121d5a141aaf693
bacpd <phamducbachvan@gmail.com>
2024-10-16 10:46:30 +0700

    first commit

---
+++ src/main/java/com/envict/secure_programming/SecureProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.envict.secure_programming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureProgrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/envict/secure_programming/SecureProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.envict.secure_programming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
